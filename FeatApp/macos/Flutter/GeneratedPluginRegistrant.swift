1b28a4f66e2ffe3853933162770f5c890df0c712
예준 <yejun4831@gmail.com>
2024-09-26 23:24:35 +0900

    .

--- FeatApp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ FeatApp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,13 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayers_darwin
+import audioplayers
 import file_selector_macos
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
